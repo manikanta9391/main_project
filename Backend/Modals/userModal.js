96b2e7015383fa2ff0d6fd47f8d7f64923cd1193
manikanta9391 <addalamanikanta1232@gmail.com>
2025-03-10 10:15:29 +0530

    fifth commit

--- Backend/Modals/userModal.js
+++ Backend/Modals/userModal.js
@@ -1,13 +1,38 @@
 import mongoose from "mongoose";
+const userSchem = {
+  Username: {
+    type: String,
+    trim: true,
+    required:true,
+    unique:true
+  },
+  Name: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  Password: {
+    type: String,
+    trim: true,
+    required: true,
+    minlength:8
+  },
+  RePassword: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  Gmail: {
+    type: String,
+    unique: true,
+    trim: true,
+    required: true,
+  }
+};
 
-const user={
-    UserName:{
-        type:String,
-        required:true,
-        unique:true
-    }
-}
+const userSchema = new mongoose.Schema(userSchem);
 
-const userSchema=new mongoose.Schema(user)
-const userModal=mongoose.model("userData",userSchema)
-export default userModal
+// Create the user model
+const userModal = mongoose.model("userLogins", userSchema);
+
+export default userModal;
